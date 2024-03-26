042f49df323854fd1f6b30e740f51fb114faabe2
RamexDeltaXOO <walid.bko47@gmail.com>
2024-03-26 16:10:57 +0100

    navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { LINKS } from "../utils";
+import { Tooltip } from "@mui/material";
 
 export default function Sidebar() {
   return (
-    <nav className='h-screen w-[70px] bg-white'>
-        <h1>Sidebar</h1>
+    <nav className="h-screen w-[70px] bg-white flex flex-col items-center gap-3">
+      <div className="w-[40px] py-4">
+        <Link to="/">
+          <img src="img/logo.png" alt="" />
+        </Link>
+        <div className="w-full h-[1px] bg-gray-200 mt-5"></div>
+      </div>
+      <div className="flex flex-col gap-6">
+        {LINKS.map((link, i) => (
+          <ul key={i}>
+            <Tooltip  title={link.name} placement="right-start">
+              <Link to={link.link} className="text-gray-400">
+                {React.createElement(link.icon, { size: 19 })}
+              </Link>
+            </Tooltip>
+          </ul>
+        ))}
+      </div>
     </nav>
-  )
+  );
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+import { Link } from "@/type";
+import { Home, Pen, Truck } from "lucide-react";
+
+export const LINKS: Link[] = [
+  {
+    name: "Accueil",
+    link: "/",
+    icon : Home
+  },
+  {
+    name: "Créer un produit",
+    link: "/create-product",
+    icon : Pen
+  },
+  {
+    name: "Liste des fournisseurs",
+    link: "/",
+    icon : Truck
+  },
+  
+];
