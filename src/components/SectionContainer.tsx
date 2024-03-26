02698f9e3df675cfd0461a94a2a8778eb75d62bb
Vincent Coccina <v.coccina@acs2i.fr>
2024-03-26 16:10:57 +0100

    navbar

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,8 +1,34 @@
-
-export default function SectionContainer({children} : {children : React.ReactNode}) {
-    return (
-      <section className="h-screen w-full bg-red-500">
-        {children}
-      </section>
-    )
-  }
+export default function SectionContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section
+      className="relative h-screen w-full"
+      style={{
+        backgroundColor: "#f1f1f1",
+        minHeight: "100vh",
+        overflow: "auto",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div
+        className="fixed top-0 w-100 h-screen"
+        style={{ overflow: "hidden" }}
+      >
+        <img
+          src="img/background_forest.jpg"
+          className="w-100 h-screen"
+          style={{
+            ["object-fit"]: "cover",
+            opacity: "0.4",
+            filter: "grayscale(100%)",
+          }}
+        />
+      </div>
+      {children}
+    </section>
+  );
+}
