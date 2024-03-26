39fe3948d502399a7aedd0f3479157b605518a58
Vincent Coccina <v.coccina@acs2i.fr>
2024-03-26 17:58:54 +0100

    header

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { LINKS } from "../utils";
 import { Tooltip } from "@mui/material";
 
 export default function Sidebar() {
+  const location = useLocation();
   return (
     <nav className="h-screen w-[70px] bg-white flex flex-col items-center gap-3">
       <div className="w-[40px] py-4">
@@ -12,17 +13,22 @@ export default function Sidebar() {
         </Link>
         <div className="w-full h-[1px] bg-gray-200 mt-5"></div>
       </div>
-      <div className="flex flex-col gap-6">
+      <ul className="flex flex-col gap-6">
         {LINKS.map((link, i) => (
-          <ul key={i}>
-            <Tooltip  title={link.name} placement="right-start">
-              <Link to={link.link} className="text-gray-400">
+            <Tooltip title={link.name} placement="right-start">
+              <Link
+                to={link.link}
+                className={`text-gray-400 ${
+                  location.pathname === link.link ? "text-orange-400" : ""
+                } ${
+                    location.pathname != link.link ? "hover:text-orange-300" : ""
+                  }`}
+              >
                 {React.createElement(link.icon, { size: 19 })}
               </Link>
             </Tooltip>
-          </ul>
         ))}
-      </div>
+      </ul>
     </nav>
   );
 }
