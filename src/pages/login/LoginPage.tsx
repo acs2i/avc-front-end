c3e88d75be684e2359d29a79839664c1b266e53f
Vincent Coccina <v.coccina@acs2i.fr>
2024-03-27 14:34:08 +0100

    fix

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import Input from "@/src/components/FormElements/Input";
+import Input from "../../components/FormElements/Input";
 
 export default function LoginPage() {
 
