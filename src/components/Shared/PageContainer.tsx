e7e858623fd81c4aa12c53159a7e196bbcc94320
Vincent Coccina <v.coccina@orange.fr>
2024-05-28 09:07:41 +0200

    retouche largeur

--- src/components/Shared/PageContainer.tsx
+++ src/components/Shared/PageContainer.tsx
@@ -1,6 +1,6 @@
 export default function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto w-[400px] md:w-[600px] lg:w-[800px] xl:w-[1200px]">
+    <div className="container mx-auto w-[400px] md:w-[600px] lg:w-[800px] xl:w-[1300px]">
       {children}
     </div>
   )
