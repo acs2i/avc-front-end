01fa79541912d923548f2a82d58dda4978752b2c
RamexDeltaXOO <walid.bko47@gmail.com>
2024-12-10 16:43:22 +0100

    fix historique

--- src/pages/params/BrandUpdatePage.tsx
+++ src/pages/params/BrandUpdatePage.tsx
@@ -49,11 +49,11 @@ export default function BrandUpdatePage({
     setFormData((prev) => ({ ...prev, [id]: value }));
   };
 
-  const handleUpdate = async (isStatusUpdate = false, newStatus = '') => {
+  const handleUpdate = async (isStatusUpdate = false, newStatus = "") => {
     setIsLoading(true);
 
     const updatedData = isStatusUpdate
-      ? { status: newStatus || (formData.status === 'A' ? 'I' : 'A') }
+      ? { status: newStatus || (formData.status === "A" ? "I" : "A") }
       : Object.entries(formData).reduce((acc, [key, value]) => {
           if (value !== selectedBrand[key as keyof Brand]) {
             acc[key as keyof Brand] = value;
@@ -81,9 +81,10 @@ export default function BrandUpdatePage({
       if (response.ok) {
         let message = "Marque modifiée avec succès !";
         if (isStatusUpdate) {
-          message = updatedData.status === 'A' 
-            ? "Marque réactivée avec succès !" 
-            : "Marque désactivée avec succès !";
+          message =
+            updatedData.status === "A"
+              ? "Marque réactivée avec succès !"
+              : "Marque désactivée avec succès !";
         }
         notifySuccess(message);
         setIsModify(false);
@@ -94,9 +95,10 @@ export default function BrandUpdatePage({
         console.error("Error response:", errorData);
         let errorMessage = "Erreur lors de la modification";
         if (isStatusUpdate) {
-          errorMessage = updatedData.status === 'A'
-            ? "Erreur lors de la réactivation"
-            : "Erreur lors de la désactivation";
+          errorMessage =
+            updatedData.status === "A"
+              ? "Erreur lors de la réactivation"
+              : "Erreur lors de la désactivation";
         }
         notifyError(errorMessage);
       }
@@ -121,18 +123,24 @@ export default function BrandUpdatePage({
           </h1>
         </div>
         <div className="flex items-center gap-4">
-          {!isModify && formData.status !== 'I' && (
+          {!isModify && formData.status !== "I" && (
             <div onClick={() => setIsModify(true)} className="cursor-pointer">
               <span className="text-[12px] text-blue-500">Modifier</span>
             </div>
           )}
-          {formData.status === 'I' && (
-            <div onClick={() => handleUpdate(true, 'A')} className="cursor-pointer">
+          {formData.status === "I" && (
+            <div
+              onClick={() => handleUpdate(true, "A")}
+              className="cursor-pointer"
+            >
               <span className="text-[12px] text-green-500">Réactiver</span>
             </div>
           )}
-          {formData.status === 'A' && (
-            <div onClick={() => handleUpdate(true, 'I')} className="cursor-pointer">
+          {formData.status === "A" && (
+            <div
+              onClick={() => handleUpdate(true, "I")}
+              className="cursor-pointer"
+            >
               <span className="text-[12px] text-red-500">Désactiver</span>
             </div>
           )}
@@ -165,7 +173,7 @@ export default function BrandUpdatePage({
             create
             onChange={handleInputChange}
             gray
-            disabled={!isModify || formData.status === 'I'}
+            disabled={!isModify || formData.status === "I"}
           />
         </div>
       </div>
@@ -220,7 +228,9 @@ export default function BrandUpdatePage({
                     <div className="text-[13px] text-gray-500 font-[500]">
                       {Object.entries(update.changes).map(([key, value]) => (
                         <p key={key}>
-                          Modification du Libellé en{" "}
+                          {value === "A" || value === "I"
+                            ? `Modification du Statut en `
+                            : `Modification du Libellé en `}
                           <span className="font-bold">{String(value)}</span>
                         </p>
                       ))}
